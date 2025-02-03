b4dabe82afc881f5240d3b4ef7e49a1bf25ce13f
nahyun park <pnh13584@gmail.com>
2025-02-03 15:06:13 +0900

    ex1 라우터 설정 완료

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Detail = () => {
+    const params = useParams();
+    console.log("params =>", params)
+    return (
+        <div>Detail</div>
+    );
+}
+ 
+export default Detail;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <div>Home</div>
+    <Link to="/Detail">Detail 페이지로 이동</Link>
+        </div>
+    
+    )
+};
+
+export default Home;
